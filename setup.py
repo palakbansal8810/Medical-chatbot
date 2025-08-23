d04a0d447c0fbe0f47bd6d07e8eea2340b9a75a1
palakbansal8810 <140965358+palakbansal8810@users.noreply.github.com>
2025-08-23 17:15:14 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='MEDICAL CHATBOT',
+    version='0.0.0',
+    author='Palak Bansal',
+    author_email='palakbansal8810@gmail.com',
+    install_requires=[]
+)
